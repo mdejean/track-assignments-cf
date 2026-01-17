6220b43c63f8eb3e599f1029c425470fac3a5007
Marcel Dejean <marcel@dejean.nyc>
2026-01-17 00:10:12 -0500

    slightly more correct durable objecting

--- src/njt_token.js
+++ src/njt_token.js
@@ -1,4 +1,4 @@
-import { DurableObject } from "cloudflare:workers";
+import { DurableObject, env } from "cloudflare:workers";
 
 import {
   getCiphers,
@@ -8,19 +8,18 @@ import {
 export class NJTToken extends DurableObject {
     constructor(ctx, env) {
         super(ctx, env);
-        this.token_payload = env.NJT_TOKEN_PAYLOAD;
-        this.token_key = env.NJT_TOKEN_KEY;
-        this.token_api = env.NJT_TOKEN_API;
+        this.token_date = this.ctx.storage.kv.get("token_date");
+        this.token = this.ctx.storage.kv.get("token");
     }
     
     async get_token() {
-        if (Date.now() - (this.ctx.storage.kv.get("token_date") || 0) > 3 * 60 * 60 * 1000) {
-            let cipher = createCipheriv('aes-192-ecb', this.token_key, '');
-            let encrypted = cipher.update("timestamp==" + new Date().toISOString() + this.token_payload, 'utf8', 'latin1');
+        if (Date.now() - (this.token_date || 0) > 3 * 60 * 60 * 1000) {
+            let cipher = createCipheriv('aes-192-ecb', env.NJT_TOKEN_KEY, '');
+            let encrypted = cipher.update("timestamp==" + new Date().toISOString() + env.NJT_TOKEN_PAYLOAD, 'utf8', 'latin1');
             encrypted += cipher.final('latin1');
             let token_fd = new FormData();
             token_fd.append("BaseInfo", btoa(encrypted));
-            let token_req = await fetch(this.token_api,
+            let token_req = await fetch(env.NJT_TOKEN_API,
                 {
                     "method": "POST",
                     "body": token_fd,
@@ -29,12 +28,14 @@ export class NJTToken extends DurableObject {
                 console.log(`NJT Token Got HTTP ${token_req.status}`);
             } else {
                 let token_resp = await token_req.json();
-                this.ctx.storage.kv.put("token", token_resp.UserToken);
-                this.ctx.storage.kv.put("token_date", Date.now());
+                this.token = token_resp.UserToken;
+                this.ctx.storage.kv.put("token", this.token);
+                this.token_date = Date.now();
+                this.ctx.storage.kv.put("token_date", this.token_date);
                 console.log("Got new token " + token_resp.UserToken);
                 return token_resp.UserToken;
             }
         }
-        return this.ctx.storage.kv.get("token");
+        return this.token;
     }
 }
